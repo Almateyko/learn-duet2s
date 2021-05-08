e7848557f7144b33830c55275b37f4388879cff9
Almateyko <olekasndr.mateyko@krpd.edu.ua>
2021-05-08 23:23:21 +0300

    Second commit

---
+++ src/main_project.py
@@ -0,0 +1,8 @@
+# -- coding utf-8 --
+__author__ = 'Alexander Mateyko'
+
+from tkinter import *
+import keyring
+
+
+
---
+++ tests/test_main_project.py
@@ -0,0 +1,6 @@
+# -- coding utf-8 --
+__author__ = 'Alexander Mateyko'
+
+from tkinter import *
+import keyring
+
